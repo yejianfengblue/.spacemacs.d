58e334a9cb25b0e861c9345119c344b9deaa8aa8
Jian Feng Ye <yejianfengblue@163.com>
2021-06-06 20:42:07 +0800

    Set font to JetBrains Mono size 14

--- init.el
+++ init.el
@@ -238,8 +238,8 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 10.0
+   dotspacemacs-default-font '("JetBrains Mono"
+                               :size 14.0
                                :weight normal
                                :width normal)
 
