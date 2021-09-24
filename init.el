b0d286cde99b942caa2ac4f1d749565324e1de69
Blue Ye <yejianfengblue@163.com>
2021-09-24 16:43:09 +0800

    Disable syntax-checking tooltips

--- init.el
+++ init.el
@@ -126,7 +126,8 @@ This function should only modify configuration layer settings."
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      spell-checking
-     syntax-checking
+     (syntax-checking :variables
+                      syntax-checking-enable-tooltips nil)
      ;; version-control
      themes-megapack
      treemacs)
