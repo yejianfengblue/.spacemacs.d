a232d0dcefc63b20aad349aee2412d4d825532a2
Blue Ye <yejianfengblue@163.com>
2021-11-11 13:50:09 +0800

    Add layer command-log

--- init.el
+++ init.el
@@ -39,6 +39,7 @@ This function should only modify configuration layer settings."
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      auto-completion
+     command-log
      ;; better-defaults
      (chinese :variables
               chinese-use-fcitx5 t
