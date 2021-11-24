6b0d86c44b504ea6e6548eb3525bf2b0b271aa1d
Blue Ye <yejianfengblue@163.com>
2021-11-24 13:58:25 +0800

    Tweak auto-completion

--- init.el
+++ init.el
@@ -38,7 +38,9 @@ This function should only modify configuration layer settings."
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     auto-completion
+     (auto-completion :variables
+                      auto-completion-tab-key-behavior 'complete
+                      auto-completion-enable-snippets-in-popup t)
      command-log
      ;; better-defaults
      (chinese :variables
