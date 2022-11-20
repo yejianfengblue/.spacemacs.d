936a64732cbc2cfe20f05218f230555dd4949e09
yejianfengblue <yejianfengblue@hotmail.com>
2022-11-20 12:18:34 +0800

    Fix fcitx

--- init.el
+++ init.el
@@ -721,7 +721,7 @@ before packages are loaded."
   (setq fcitx-active-evil-states '(insert emacs hybrid)) ; if you use hybrid mode
   (fcitx-aggressive-setup)
   (fcitx-prefix-keys-add "M-m") ; M-m is common in Spacemacs
-  (setq fcitx-use-dbus t) ; uncomment if you're using Linux
+  ;; (setq fcitx-use-dbus t) ; uncomment if you're using Linux
 
   (with-eval-after-load 'org)
 
