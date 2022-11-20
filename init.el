11018eec458790ecd8053be61cc36237625ea2c5
yejianfengblue <yejianfengblue@hotmail.com>
2022-11-20 12:11:10 +0800

    Toggle camel case motion globally on

--- init.el
+++ init.el
@@ -677,6 +677,7 @@ Put your configuration code here, except for variables that should be set
 before packages are loaded."
   ;; (if (display-graphic-p)
   (spacemacs//set-monospaced-font "MesloLGS NF" "文泉驿微米黑" 18 22)
+  (spacemacs/toggle-camel-case-motion-globally-on)
   ;; )
 
 
